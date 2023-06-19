07f9bcefaa1423d7778443374f6665f39ce891b6
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-06-19 15:44:43 +0300

    Initial Commit

---
+++ Right on Target SwiftUI/Right_on_Target_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Right_on_Target_SwiftUIApp.swift
+//  Right on Target SwiftUI
+//
+//  Created by Kate Kashko on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Right_on_Target_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
